4d9317ab80e055d10b8e4afa5f2dd939a16566c0
Lucas <51488@edu.raseko.fi>
2024-02-06 13:21:54 +0200

    Started creating a new class for pet owner. Basic fields designed

---
+++ LuokkaHarjoitus1/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LuokkaHarjoitus1
+{
+    class Hooman
+    {
+        // Define properties of hooman
+        string name;
+        int age;
+        string gender;
+    }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
