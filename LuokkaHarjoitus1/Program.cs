a6f73dca8a7fd05cdb1e710e6d14a87030dc3b27
Lucas <51488@edu.raseko.fi>
2024-02-06 14:29:57 +0200

    Added a constructor with 3 over loads.

--- LuokkaHarjoitus1/Program.cs
+++ LuokkaHarjoitus1/Program.cs
@@ -8,10 +8,42 @@ namespace LuokkaHarjoitus1
 {
     class Hooman
     {
-        // Define properties of hooman
-        string name;
-        int age;
-        string gender;
+        // Define properties of hooman ie. fields
+        string name = "Essi esimerkki";
+        int age = 30;
+        string gender = "Emäntä";
+
+        // Constructor with 
+
+        Hooman()
+        { 
+            
+        }
+
+        // Constructor with one argument
+        Hooman(string name)
+        {
+            this.name = name;
+        }
+
+        // Constructor with 2 arguments
+
+        Hooman(string name, int age)
+        {
+            this.name = name;
+            this.age = age;
+        }
+
+        // Constructor with 3 arguments
+
+        Hooman(string name, int age, string gender)
+        {
+            this.name = name;
+            this.age = age;
+            this.gender = gender;
+
+        }
+
     }
     internal class Program
     {
